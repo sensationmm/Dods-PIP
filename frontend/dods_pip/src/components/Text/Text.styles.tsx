8f6075a898770da9798a55516f645ca2892eeade
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2021-08-09 16:14:40 -0600

    DOD-205: Typography line heights

--- frontend/dods_pip/src/components/Text/Text.styles.tsx
+++ frontend/dods_pip/src/components/Text/Text.styles.tsx
@@ -4,92 +4,103 @@ import mediaQueries from '../../globals/media';
 export const h1 = styled.h1`
   font-family: 'Libre Baskerville';
   font-size: 64px;
-  line-height: 80px;
+  line-height: 64px;
   padding: 0;
   margin: 0;
 
   ${mediaQueries('md')`
-    font-size: 58px;
+    font-size: 58px;    
+    line-height: 58px;
   `}
 
   ${mediaQueries('xl')`
-    font-size: 74px;
+    font-size: 74px;    
+    line-height: 74px;
   `}
 
   ${mediaQueries('xxl')`
-    font-size: 80px;
+    font-size: 80px;    
+    line-height: 80px;
   `}
 `;
 
 export const h2 = styled.h2`
   font-family: 'Libre Baskerville';
   font-size: 34px;
-  line-height: 64px;
+  line-height: 34px;
   padding: 0;
   margin: 0;
 
   ${mediaQueries('md')`
     font-size: 42px;
+    line-height: 42px;
   `}
 
   ${mediaQueries('lg')`
     font-size: 52px;
+    line-height: 52px;
   `}
 
   ${mediaQueries('xl')`
     font-size: 64px;
+    line-height: 64px;
   `}
 `;
 
 export const h3 = styled.h3`
   font-family: 'Libre Baskerville';
   font-size: 32px;
-  line-height: 48px;
+  line-height: 32px;
   padding: 0;
   margin: 0;
 
   ${mediaQueries('md')`
     font-size: 38px;
+    line-height: 38px;
   `}
 
   ${mediaQueries('lg')`
     font-size: 48px;
+    line-height: 48px;
   `}
 `;
 
 export const h4 = styled.h4`
   font-family: 'Libre Baskerville';
   font-size: 24px;
-  line-height: 32px;
+  line-height: 24px;
   padding: 0;
   margin: 0;
 
   ${mediaQueries('lg')`
     font-size: 32px;
+    line-height: 32px;
   `}
 `;
 
 export const h5 = styled.h5`
   font-family: 'Libre Baskerville';
   font-size: 18px;
-  line-height: 24px;
+  line-height: 18px;
   padding: 0;
   margin: 0;
 
   ${mediaQueries('lg')`
     font-size: 24px;
+    line-height: 24px;
   `}
 `;
 
 export const h6 = styled.h6`
   font-family: 'Libre Baskerville';
   font-size: 14px;
-  line-height: 18px;
+  line-height: 14px;
   padding: 0;
   margin: 0;
 
   ${mediaQueries('lg')`
     font-size: 18px;
+    line-height: 18px;
   `}
 `;
 
