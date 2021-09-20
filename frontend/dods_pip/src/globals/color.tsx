8d01d4500ba8b38c42ecab5084411e746c2ba9c8
LuisGSandoval <luis.sandoval.457@outlook.com>
2021-09-20 08:36:06 -0500

    Feat(DOD): Adding latest colors to update current components

--- frontend/dods_pip/src/globals/color.tsx
+++ frontend/dods_pip/src/globals/color.tsx
@@ -2,29 +2,36 @@ const color = {
   theme: {
     blueLight: '#3776CA',
     blueMid: '#124384',
-    blue: '#222955',
+    blue: '#122035',
     blueDark: '#122035',
+    bluePastel: '#3776CA',
+    blueSky: '#3776CA',
   },
   shadow: {
     blue: '#EAEEF3',
-    grey: '#D9D9D9',
+    grey: '#F5F5F6',
     red: '#f9ecea',
     yellow: '#FFF8E5',
   },
   base: {
-    white: '#FDFDFD',
+    white: '#FFFFFF',
     ivory: '#F8F8F8',
+    greyLighter: '#F3F3F3',
+    greyLight: '#E5E5E5',
+    greyMid: '#C7C7C7',
     grey: '#9D9D9F',
+    greyDark: '#757575',
     black: '#161616',
   },
   accent: {
     yellow: '#F9B400',
     orange: '#DD7B00',
+    green: '#29C26E',
   },
   alert: {
     green: '#17824E',
-    greenLight: '#29C26E',
-    red: '#EF4E3B',
+    orange: '#AF6400',
+    red: '#EB1413',
   },
 };
 
