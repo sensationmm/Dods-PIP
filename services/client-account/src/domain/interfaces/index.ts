ef34829dbcf14a899897e7fec123e21aae3bfa55
Leo Prada <leo.prada90@gmail.com>
2021-09-22 14:19:31 -0500

    Export Pagination to interfaces

--- services/client-account/src/domain/interfaces/index.ts
+++ services/client-account/src/domain/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './ClientAccountPersister';
 export * from './ClientAccount';
 export * from './SubscriptionType';
 export * from './SubscriptionTypePersister';
+export * from './Pagination';
