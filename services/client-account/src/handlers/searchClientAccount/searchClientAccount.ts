2c50bf32dbe7c75ca9218df55e2d2d2d044c450c
Leo Prada <leo.prada90@gmail.com>
2021-09-22 01:31:42 -0500

    remove log

--- services/client-account/src/handlers/searchClientAccount/searchClientAccount.ts
+++ services/client-account/src/handlers/searchClientAccount/searchClientAccount.ts
@@ -5,7 +5,7 @@ import { SearchClientAccountParameters, HttpSuccessResponse } from '../../domain
 // export const searchClientAccount = async ({ clientAccountId, }: { clientAccountId: string; }): Promise<APIGatewayProxyResultV2> => {
 export const searchClientAccount = async (params: SearchClientAccountParameters): Promise<APIGatewayProxyResultV2> =>
 {
-    console.log('params :', params)
+    // console.log('params :', params)
     const response = await ClientAccountRepository.defaultInstance.searchClientAccount(params);
 
     return new HttpSuccessResponse(
