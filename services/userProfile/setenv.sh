e4624748ab57912ca6a41661a2492dc9f2c45b73
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-09-29 06:13:58 -0500

    Replace running a shell and using 'source'

--- services/userProfile/setenv.sh
+++ services/userProfile/setenv.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+# Temporary solution since we can't keep adding these by hand here.
 export SERVERLESS_STAGE=dev
 export SERVERLESS_PORT=3000
 export GET_USER_ENDPOINT=http://localhost:3000/dev/user
