bee467add1ec9e5f4a4aaed3dc9886d9845ea3d6
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-09-29 05:39:27 -0500

    Fixed shell used on alpine based images

--- services/userProfile/setenv.sh
+++ services/userProfile/setenv.sh
@@ -1,8 +1,7 @@
-#! /usr/bin/env bash
+#! /bin/sh
 
 export SERVERLESS_STAGE=dev
 export SERVERLESS_PORT=3000
 export GET_USER_ENDPOINT=http://localhost:3000/dev/user
 export GET_USERBYNAME_ENDPOINT=http://localhost:3000/dev/userByName
 export GET_ROLE_ENDPOINT=http://localhost:3000/dev/role
-
