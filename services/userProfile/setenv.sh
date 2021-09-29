6dfb25e1216d106b9c4501d3771f007d07bd2d7e
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-09-29 05:30:08 -0500

    Setting env vars required by boilerplate tests

---
+++ services/userProfile/setenv.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+export SERVERLESS_STAGE=dev
+export SERVERLESS_PORT=3000
+export GET_USER_ENDPOINT=http://localhost:3000/dev/user
+export GET_USERBYNAME_ENDPOINT=http://localhost:3000/dev/userByName
+export GET_ROLE_ENDPOINT=http://localhost:3000/dev/role
+
