d4e94856ea2f7a4e4e6c14392c08fe7940e3ca03
Diego <diego.avellaneda@somoglobal.com>
2021-10-04 20:54:16 -0500

    Coverage Threshold lowered to 40%

--- services/client-account/jest.config.js
+++ services/client-account/jest.config.js
@@ -15,10 +15,10 @@ module.exports = {
     coverageReporters: ['json', 'lcov', 'text', 'cobertura'],
     coverageThreshold: {
         global: {
-            branches: 60,
-            functions: 60,
-            lines: 60,
-            statements: 60,
+            branches: 40,
+            functions: 40,
+            lines: 40,
+            statements: 40,
         },
     },
 };
