3f336aad87bd8821fc296be41ac5466d8e62a220
michael-brown-somo <michael.brown@somoglobal.com>
2021-10-11 14:36:46 +0100

    tidying

--- infrastructure-terraform/main.tf
+++ infrastructure-terraform/main.tf
@@ -2,7 +2,7 @@
 // - Dev environment
 // --------------------------------------------------------------------------------------------------------------------
 module "development" {
-  source      = "./deployments/environment-deployment"
+  source      = "./environments/deployment"
   environment = "development"
   app_image   = "390773179818.dkr.ecr.eu-west-1.amazonaws.com/dods-pip/frontend-dods-pip:639732e4f367758bb6eddb333fc5f620b4d80029"
   account_id  = "390773179818"
