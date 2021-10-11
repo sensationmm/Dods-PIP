dbcbd1a33a07b1748e7fa3cd389a1541c62c1b14
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-10-11 14:08:42 -0500

    Revert "Removed script meant for Bazel from current pipeline"
    This reverts commit 9c4214083db770d5fd89d437307751a5e542881b.

--- services/client-account/src/handlers/checkClientAccountName/index.ts
+++
@@ -1,9 +0,0 @@
-import { buildLambdaFunction } from '@dodsgroup/dods-lambda';
-import { checkClientAccountName } from './checkClientAccountName';
-import { config } from '../../domain';
-
-export const handle = buildLambdaFunction(checkClientAccountName, {
-    openApiDocumentPath: config.openApiPath,
-    validateRequests: true,
-    validateResponses: false,
-});
