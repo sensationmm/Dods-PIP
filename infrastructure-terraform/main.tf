9bbe44a84a163c57c843d75334413ccb8036b44d
michael-brown-somo <michael.brown@somoglobal.com>
2021-10-12 09:05:01 +0100

    spelling correction

--- infrastructure-terraform/main.tf
+++ infrastructure-terraform/main.tf
@@ -6,7 +6,20 @@ module "development" {
   environment = "development"
   app_image   = "390773179818.dkr.ecr.eu-west-1.amazonaws.com/dods-pip/frontend-dods-pip:639732e4f367758bb6eddb333fc5f620b4d80029"
   account_id  = "390773179818"
-
+  providers = {
+    aws = aws.dev
+  }
 }
 
-
+// --------------------------------------------------------------------------------------------------------------------
+// - prod environment
+// --------------------------------------------------------------------------------------------------------------------
+module "production" {
+  source      = "./environments/deployment"
+  environment = "production"
+  app_image   = "186202231680.dkr.ecr.eu-west-1.amazonaws.com/dods-pip/frontend-dods-pip:" //non existent
+  account_id  = "186202231680"
+  providers = {
+    aws = aws.prod
+  }
+}
