ef4870583475de9591bb51f3c7e89adb751be046
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-10-15 13:53:49 -0500

    Fix security group for lamdbas exported as js files

--- infrastructure-terraform/environments/deployment/env_configs.tf
+++ infrastructure-terraform/environments/deployment/env_configs.tf
@@ -1,13 +1,15 @@
 data "template_file" "vpc_config_dev" {
   template = file("${path.module}/../../templates/vpc.js.tpl")
   vars = {
-    securityGroupIds = jsonencode([module.client-profile-database.db_security_group])
-    subnetIds = jsonencode(module.vpc.private_subnet_ids)
+    securityGroupIds = jsonencode([module.client-profile-database.lambda_sg_id])
+    subnetIds = jsonencode(module.vpc.public_subnet_ids)
   }
 }
 
 resource "local_file" "vpc_config" {
   filename = "${path.module}/../../vpc.${var.environment}.js"
   content  = data.template_file.vpc_config_dev.rendered
+
+
 }
 
--- infrastructure-terraform/environments/deployment/services/vpc/main.tf
+++ infrastructure-terraform/environments/deployment/services/vpc/main.tf
@@ -319,6 +319,12 @@ resource "aws_route_table" "private" {
 
 }
 
+#resource "aws_route" "private" {
+#  route_table_id = aws_route_table.private.id
+#  destination_cidr_block = "0.0.0.0/0"
+#  nat_gateway_id = aws_egress_only_internet_gateway.eigw.id
+#}
+
 // - Explicitly associate the newly created route tables to the private subnets (so they don't default to the main route table)
 resource "aws_route_table_association" "private" {
   count          = var.az_count
