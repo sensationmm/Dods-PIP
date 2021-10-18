3a12221842aa72c430dd8e06f47e1d301fa2d3b3
Jose Guerra <jose.guerra@dodsgroup.com>
2021-10-18 11:43:07 +0100

    added hover classes

--- frontend/dods_pip/src/components/Tooltip/index.tsx
+++ frontend/dods_pip/src/components/Tooltip/index.tsx
@@ -1,56 +1,83 @@
-import classNames from 'classnames'
-import Text from '../Text'
-import * as Styled from './Tooltip.styles'
-import Icon, { IconSize } from '../Icon'
-import { Icons } from '../Icon/assets'
-import color from '../../globals/color'
+import classNames from 'classnames';
 
-export type alignmentType = 'topLeft' | 'topRight' | 'right'
-export type colorMode = 'Light' | 'Dark'
+import color from '../../globals/color';
+import Icon, { IconSize } from '../Icon';
+import { Icons } from '../Icon/assets';
+import Text from '../Text';
+import * as Styled from './Tooltip.styles';
+
+export type alignmentType = 'topLeft' | 'topRight' | 'right';
+export type colorMode = 'Light' | 'Dark';
 
 export interface TooltipProps {
-  alignment?: alignmentType
-  colorType?: colorMode
-  title?: string
-  body?: string
-  icon?: Icons
-  trigger?: JSX.Element
-  classShow?: boolean
+  alignment?: alignmentType;
+  colorType?: colorMode;
+  title?: string;
+  body?: string;
+  icon?: Icons;
+  trigger?: JSX.Element;
+  classShow?: boolean;
+  classHover?: boolean;
 }
 
-const Tooltip: React.FC<TooltipProps> = ({alignment, colorType, title, body, icon, trigger, classShow}) => {
-  let Component = Styled.tooltipStyle
+const Tooltip: React.FC<TooltipProps> = ({
+  alignment,
+  colorType,
+  title,
+  body,
+  icon,
+  trigger,
+  classShow,
+  classHover,
+}) => {
+  const Component = Styled.tooltipStyle;
 
   return (
     <Styled.wrapper data-test="component-tooltip">
       {trigger && trigger}
-      <Component className={classNames({
-        alignTopLeft: alignment === 'topLeft',
-        alignTopRight: alignment === 'topRight',
-        alignRight: alignment === 'right',
-        colorLight: colorType === 'Light',
-        colorDark: colorType === 'Dark',
-        show: classShow ? 'show' : '',
-        hover: 'hover'
-      })}>
+
+      <Component
+        className={classNames({
+          alignTopLeft: alignment === 'topLeft',
+          alignTopRight: alignment === 'topRight',
+          alignRight: alignment === 'right',
+          colorLight: colorType === 'Light',
+          colorDark: colorType === 'Dark',
+          show: classShow ? 'show' : '',
+          hover: classHover ? '' : 'hover',
+          popover: 'popover',
+        })}
+      >
         <div className="inner">
-          {title && <Text data-test="component-title" color={colorType === 'Dark' ? color.base.white : color.theme.blue} type="body">{title}</Text>}
+          {title && (
+            <Text
+              data-test="component-title"
+              color={colorType === 'Dark' ? color.base.white : color.theme.blue}
+              type="body"
+            >
+              {title}
+            </Text>
+          )}
+          (
           <Text
             data-test="component-body"
-            color={
-              colorType === 'Dark' ? 
-              color.base.white : 
-              color.theme.blue
-            }
-            type='bodySmall'
+            color={colorType === 'Dark' ? color.base.white : color.theme.blue}
+            type="bodySmall"
           >
-            {icon && <Icon src={icon} size={IconSize.medium} color={colorType === 'Light' ? color.theme.blue : color.base.white} />}
+            )
+            {icon && (
+              <Icon
+                src={icon}
+                size={IconSize.medium}
+                color={colorType === 'Light' ? color.theme.blue : color.base.white}
+              />
+            )}
             {body}
           </Text>
         </div>
       </Component>
     </Styled.wrapper>
-  )
-}
+  );
+};
 
-export default Tooltip
+export default Tooltip;
