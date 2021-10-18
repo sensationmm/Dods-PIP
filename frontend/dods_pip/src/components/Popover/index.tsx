f8a2f45d93179ca9f0d9a65e4f473f6f4da86d13
Jose Guerra <jose.guerra@dodsgroup.com>
2021-10-18 11:55:27 +0100

    removed unused variable

--- frontend/dods_pip/src/components/Popover/index.tsx
+++ frontend/dods_pip/src/components/Popover/index.tsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
-import * as Styled from './Popover.styles'
-import Tooltip, { TooltipProps } from '../Tooltip'
-import { Icons } from '../Icon/assets'
-import Icon, { IconSize } from '../Icon'
+import React, { useState } from 'react';
 
-export type alignmentType = 'topLeft' | 'topRight' | 'right'
+import Icon, { IconSize } from '../Icon';
+import { Icons } from '../Icon/assets';
+import Tooltip, { TooltipProps } from '../Tooltip';
+import * as Styled from './Popover.styles';
+
+export type alignmentType = 'topLeft' | 'topRight' | 'right';
 
 export interface PopoverProps extends Omit<TooltipProps, 'trigger' | 'colorType' | 'icon'> {}
 
-const Popover: React.FC<PopoverProps> = ({...rest}) => {
-  const [show, setShow] = useState(false)
+const Popover: React.FC<PopoverProps> = ({ ...rest }) => {
+  const [show, setShow] = useState(false);
 
-  const toggleClass = () => setShow(!show)
+  const toggleClass = () => setShow(!show);
 
-  const customButton = <button className='btnTrigger' onClick={toggleClass}><Icon src={Icons.IconInfo} size={IconSize.large} /></button>
+  const customButton = (
+    <button className="btnTrigger" onClick={toggleClass}>
+      <Icon src={Icons.IconInfo} size={IconSize.large} />
+    </button>
+  );
 
   return (
     <Styled.popoverWrapper data-test="popover">
-      <Tooltip {...rest} trigger={customButton} classShow={show} ></Tooltip>
+      <Tooltip {...rest} trigger={customButton} classShow={show} classHover={true}></Tooltip>
     </Styled.popoverWrapper>
-  )
-}
+  );
+};
 
-export default Popover
+export default Popover;
