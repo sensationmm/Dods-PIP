4d4667ea53281e2a2559aaec3479d9fd194ba0bd
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2021-10-18 14:10:02 +0100

    bug(DOD-382): clearing bug

--- frontend/dods_pip/src/utils/array.tsx
+++ frontend/dods_pip/src/utils/array.tsx
@@ -1,17 +1,3 @@
 export const inArray = (item: string, array: Array<string>): boolean => {
   return array.indexOf(item) >= 0;
 };
-
-export const getIndexById = (
-  arr: Array<Record<string, unknown>>,
-  key: string,
-  value: unknown,
-): number | undefined => {
-  let index;
-  arr.forEach((obj, count) => {
-    if (obj[key] === value) {
-      index = count;
-    }
-  });
-  return index;
-};
