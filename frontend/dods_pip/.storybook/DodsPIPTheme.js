e77ca0cf89c7f56850d2049ac023d34758dfcf28
Alixe B <alixe.bhagooa+dods@somoglobal.com>
2021-10-20 14:57:20 +0000

    Merged in task/storybook-styling-fix (pull request #149) Fix storybook styling issue Approved-by: Menelik Williams

--- frontend/dods_pip/.storybook/DodsPIPTheme.js
+++ frontend/dods_pip/.storybook/DodsPIPTheme.js
@@ -1,11 +1,7 @@
 import { create } from '@storybook/theming';
 import Logo from '../src/assets/images/logo-white-dods-pip.png';
-import color from '../src/globals/color';
 
 export default create({
-  textColor: 'white',
-  appBg: color.theme.blue,
-  appContentBg: 'white',
   appBorderRadius: 2,
   brandTitle: 'Dods PIP Components',
   brandImage: Logo,
