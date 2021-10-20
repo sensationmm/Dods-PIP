6e6035e520af9dad5f6528d67205ea0ca91b43a2
michael-brown-somo <michael.brown@somoglobal.com>
2021-10-20 13:27:32 +0100

    deploy to terraformed buckets

--- infrastructure-terraform/environments/deployment/services/devtools/main.tf
+++ infrastructure-terraform/environments/deployment/services/devtools/main.tf
@@ -6,3 +6,21 @@ module "storybook" {
   environment = var.environment
   name        = "storybook"
 }
+
+// --------------------------------------------------------------------------------------------------------------------
+// - Scrapping Lambda bucket
+// --------------------------------------------------------------------------------------------------------------------
+module "scrapping" {
+  source      = "./modules/s3"
+  environment = var.environment
+  name        = "scrapping-lambdas"
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// - Services Lambda bucket
+// --------------------------------------------------------------------------------------------------------------------
+module "services" {
+  source      = "./modules/s3"
+  environment = var.environment
+  name        = "services-lambdas"
+}
