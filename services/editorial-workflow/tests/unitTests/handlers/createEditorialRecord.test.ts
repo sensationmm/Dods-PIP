e53edcf375dfd830f56373851a55b934990c63dd
jay <shakyj@gmail.com>
2021-10-21 13:04:08 +0200

    Fixed some more merge errors

--- services/editorial-workflow/tests/unitTests/handlers/createEditorialRecord.test.ts
+++ services/editorial-workflow/tests/unitTests/handlers/createEditorialRecord.test.ts
@@ -1,16 +1,21 @@
 import { createEditorialRecord } from '../../../src/handlers/createEditorialRecord/createEditorialRecord';
-import { CreateEditorialRecordParameters } from "../../../src/domain";
-import { HttpResponse } from '@dodsgroup/dods-lambda';
+import { HttpResponse, createContext } from '@dodsgroup/dods-lambda';
 // @ts-ignore
 import expect from "expect";
 
+import { CreateEditorialRecordParameters } from "../../../src/domain";
+
+jest.mock('../../../src/dynamodb');
+
 const FUNCTION_NAME = "createEditorialRecord";
+
+const defaultContext = createContext();
 
 describe(`${FUNCTION_NAME} handler`, () => {
     test(`${FUNCTION_NAME}`, async () => {
         const data: CreateEditorialRecordParameters = { document_name: 'Some Questions', s3_location: 'Some location' };
 
-        const response = await createEditorialRecord(data);
+        const response = await createEditorialRecord(data, defaultContext);
         expect(response).toBeInstanceOf(HttpResponse);
         expect(JSON.stringify(response)).toContain("document_name")
         expect(JSON.stringify(response)).toContain("s3_location")
