177be060165a3cfd2b03bc571a14ce12cedf9bdc
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2021-10-29 15:24:16 +0100

    chore(DOD-524): Button styles

--- frontend/dods_pip/src/components/IconButton/index.tsx
+++ frontend/dods_pip/src/components/IconButton/index.tsx
@@ -2,27 +2,71 @@ import classNames from 'classnames';
 import React from 'react';
 
 import color from '../../globals/color';
+import { ButtonProps } from '../Button';
+import * as Styled from '../Button/Button.styles';
 import Icon, { IconSize } from '../Icon';
-import { Icons } from '../Icon/assets';
-import * as Styled from './IconButton.styles';
 
-export interface IconButtonProps extends React.HTMLProps<HTMLButtonElement> {
-  icon?: Icons;
-}
+export interface IconButtonProps extends Omit<ButtonProps, 'iconAlignment' | 'inline' | 'width'> {}
 
-const IconButton: React.FC<IconButtonProps> = ({ label, onClick, disabled = false, icon }) => {
+const IconButton: React.FC<IconButtonProps> = ({
+  label,
+  onClick,
+  disabled = false,
+  icon,
+  type = 'primary',
+  isSmall = false,
+}) => {
+  let Component = Styled.primary;
+
+  switch (type) {
+    case 'primary':
+      Component = Styled.primary;
+      break;
+    case 'secondary':
+      Component = Styled.secondary;
+      break;
+    case 'text':
+      Component = Styled.text;
+      break;
+  }
+
   return (
     <div data-test="component-icon-button">
-      <Styled.button
+      <Component
+        isIconButton
+        width="fixed"
+        disabled={disabled}
         className={classNames({
-          disabled: disabled,
+          small: isSmall,
           icon: icon,
         })}
         onClick={onClick}
       >
         {label}
-        {icon && !label && <Icon src={icon} size={IconSize.medium} color={color.base.white} />}
-      </Styled.button>
+        {icon && !label && (
+          <Icon
+            src={icon}
+            size={
+              isSmall
+                ? type !== 'text'
+                  ? IconSize.medium
+                  : IconSize.mediumLarge
+                : type !== 'text'
+                ? IconSize.large
+                : IconSize.xlarge
+            }
+            color={
+              type === 'primary'
+                ? !disabled
+                  ? color.base.white
+                  : color.base.greyDark
+                : !disabled
+                ? color.theme.blueMid
+                : color.base.greyDark
+            }
+          />
+        )}
+      </Component>
     </div>
   );
 };
