c4e03958bf239d75b5776fce4d508a24bc989975
kenanhancer <kenanhancer@gmail.com>
2021-11-02 14:53:06 +0000

    duplicated function and interfaces are deleted

--- services/user-profile/src/domain/interfaces/index.ts
+++ services/user-profile/src/domain/interfaces/index.ts
@@ -5,6 +5,7 @@ export * from './user-profile';
 export * from './typeParsers';
 export * from './UserProfile';
 export * from './UserProfilePersister';
+export * from './UserProfilePersisterV2';
 
 export interface DownstreamEndpoints {
     getUserEndpoint: string;
