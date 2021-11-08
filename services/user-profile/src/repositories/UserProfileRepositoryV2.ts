00e5af1276038bf21de8f77cd448e51cb56e9e58
kenan hancer <kenanhancer@gmail.com>
2021-11-08 16:51:38 +0000

    dod-935: lambda function is updated

--- services/user-profile/src/repositories/UserProfileRepositoryV2.ts
+++ services/user-profile/src/repositories/UserProfileRepositoryV2.ts
@@ -1,35 +1,56 @@
-import { User } from '@dodsgroup/dods-model';
-import { Op, WhereOptions } from 'sequelize';
+import { User, Role } from '@dodsgroup/dods-model';
+import { Op } from 'sequelize';
 
-import { SearchUsersInput, SearchUsersOutput } from '../domain';
-import { UserProfilePersisterV2 } from '../domain/interfaces/UserProfilePersisterV2';
+import { SearchUsersInput, SearchUsersOutput, UserProfileError, UserProfilePersisterV2 } from '../domain';
+
+export const LAST_NAME_COLUMN = 'lastName';
+export const ROLE_ID_COLUMN = 'roleId';
+export const ASC = 'ASC';
 
 export class UserProfileRepositoryV2 implements UserProfilePersisterV2 {
 
     static defaultInstance: UserProfilePersisterV2 = new UserProfileRepositoryV2();
 
-    async searchUsers(parameters: SearchUsersInput): Promise<Array<SearchUsersOutput>> {
+    async searchUsers(parameters: SearchUsersInput): Promise<SearchUsersOutput> {
 
-        const { name, limit, offset } = parameters;
+        const { name, startsWith, role, limit, offset, sortBy, sortDirection } = parameters;
 
-        let whereClause: WhereOptions = {
+        let whereClause: any = {
             [Op.or]: [
                 { firstName: { [Op.like]: `${name}%` } },
                 { lastName: { [Op.like]: `${name}%` } }
             ]
         };
 
-        const users = await User.findAll({
+        if (startsWith) {
+            whereClause[LAST_NAME_COLUMN] = { [Op.like]: `${startsWith}%` };
+        }
+
+        let roleRecord: Role | null;
+        if (role) {
+            roleRecord = await Role.findOne({ where: { uuid: role } });
+
+            if (!roleRecord) {
+                throw new UserProfileError(`Error: RoleUuid ${role} does not exist`);
+            }
+
+            whereClause[ROLE_ID_COLUMN] = roleRecord?.id;
+        }
+
+        const { rows, count } = await User.findAndCountAll({
             where: whereClause,
             subQuery: false,
             include: [User.associations.role],
             order: [
-                ['firstName', 'ASC'],
+                [sortBy!, sortDirection!],
             ],
-            offset: offset,
-            limit: limit,
+            offset: offset!,
+            limit: limit!,
         });
 
-        return users.map(({ uuid, firstName, lastName }) => ({ uuid, firstName, lastName }));
+        return {
+            users: rows.map(({ uuid, firstName, lastName, primaryEmail }) => ({ uuid, firstName, lastName, email: primaryEmail, role: roleRecord!.title })),
+            count
+        };
     }
 }
