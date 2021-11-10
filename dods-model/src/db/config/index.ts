5c53ac76a606ab1e1228b03f0184a26362bac22c
kenan hancer <kenanhancer@gmail.com>
2021-11-10 20:23:34 +0000

    dod-935: dods-models is udpated for env validation

--- dods-model/src/db/config/index.ts
+++ dods-model/src/db/config/index.ts
@@ -1,26 +1,68 @@
 require('dotenv').config();
 
-import { Dialect } from 'sequelize';
+import Joi, { Schema } from 'joi';
 import mariaDb from 'mariadb';
+import { Dialect } from 'sequelize';
 
-const nodeEnv = process.env.NODE_ENV as string;
-const dbName = process.env.DB_NAME as string;
-const dbUser = process.env.DB_USER as string;
-const dbHost = process.env.DB_HOST as string;
-const dbDriver = process.env.DB_DRIVER as Dialect;
-const dbPassword = process.env.DB_PASSWORD as string;
-const dbConnectionLimit = parseInt(process.env.DB_CONNECTION_LIMIT!) || 5;
+const loadConfig = (schema: Schema) => {
+
+  const { value: envVars, error } = schema.prefs({ errors: { label: 'key' } }).validate(process.env);
+
+  if (error) {
+    console.error(`Config validation error: ${error.message}`);
+    process.exit(1);
+  }
+
+  return envVars;
+};
+
+const stages = ['production', 'development', 'test'];
+const sequelizeMetadataStorage = ['none', 'sequelize', 'json'];
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string().valid(...stages).default('development'),
+    DB_HOST: Joi.string().required(),
+    DB_NAME: Joi.string().required(),
+    DB_USER: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
+    DB_DRIVER: Joi.string().default('mariadb'),
+    DB_CONNECTION_LIMIT: Joi.number().default(5),
+    DB_SEED_STORAGE: Joi.string().valid(...sequelizeMetadataStorage).default('sequelize')
+  })
+  .unknown();
+
+const envVars = loadConfig(envVarsSchema);
+
+
+
+const config = {
+  nodeEnv: envVars.NODE_ENV as string,
+  isTestEnv: envVars.NODE_ENV !== 'test',
+  mariaDb: {
+    host: envVars.DB_HOST as string,
+    name: envVars.DB_NAME as string,
+    user: envVars.DB_USER as string,
+    password: envVars.DB_PASSWORD as string,
+    driver: envVars.DB_DRIVER as string,
+    connectionLimit: envVars.DB_CONNECTION_LIMIT as number,
+    seederStorage: envVars.DB_SEED_STORAGE as string,
+  }
+};
+
+const { nodeEnv, mariaDb: { host, name, user, password, driver, connectionLimit, seederStorage } } = config
 
 export default {
   [nodeEnv || "development"]: {
-    host: dbHost,
-    database: dbName,
-    username: dbUser,
-    password: dbPassword,
-    dialect: dbDriver,
+    host: host,
+    database: name,
+    username: user,
+    password: password,
+    dialect: driver as Dialect,
     dialectModule: mariaDb,
+    seederStorage,
     pool: {
-      max: dbConnectionLimit, // Maximum number of connection in pool
+      max: connectionLimit, // Maximum number of connection in pool
       min: 0, // Minimum number of connection in pool
       acquire: 30000, // The maximum time, in milliseconds, that pool will try to get connection before throwing error
       idle: 10000 // The maximum time, in milliseconds, that a connection can be idle before being released
