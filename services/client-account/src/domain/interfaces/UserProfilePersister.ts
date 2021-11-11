edcec57a20b65cb4d7b724ec374028503aaeadf2
Diego <diego.avellaneda@somoglobal.com>
2021-11-11 13:17:56 -0500

    verify email before creatig team member with unit test

--- services/client-account/src/domain/interfaces/UserProfilePersister.ts
+++ services/client-account/src/domain/interfaces/UserProfilePersister.ts
@@ -1,5 +1,8 @@
-import { UserProfileModel, UserProfileModelAttributes } from "../../db";
+import { UserProfileModel, UserProfileModelAttributes } from '../../db';
 
 export interface UserProfilePersister {
-    findOne(where: Partial<UserProfileModelAttributes>): Promise<UserProfileModel>;
+    findOne(
+        where: Partial<UserProfileModelAttributes>
+    ): Promise<UserProfileModel>;
+    checkUserEmailAvailability(primaryEmailAddress: string): Promise<boolean>;
 }
--- services/client-account/tests/unitTests/repositories/UserProfileRepository.test.ts
+++ services/client-account/tests/unitTests/repositories/UserProfileRepository.test.ts
@@ -1,49 +1,92 @@
-import { UserProfileRepository } from '../../../src/repositories';
 import { UserProfileModel } from '../../../src/db/models';
+import { UserProfileRepository } from '../../../src/repositories';
 import { mocked } from 'ts-jest/utils';
 
-const defaultUserProfile = {
-    id: 1,
-    uuid: 'b0605d89-6200-4861-a9d5-258ccb33cbe3',
-    firstName: 'kenan',
-    lastName: 'hancer',
-    title: 'Mr',
-    roleId: 1,
-    primaryEmail: 'k@h.com',
-} as UserProfileModel;
+const defaultUsersProfile = [
+    {
+        id: 1,
+        uuid: 'b0605d89-6200-4861-a9d5-258ccb33cbe3',
+        firstName: 'kenan',
+        lastName: 'hancer',
+        title: 'Mr',
+        roleId: 1,
+        primaryEmail: 'k@h.com',
+    } as UserProfileModel,
+    {
+        id: 2,
+        uuid: '0a11a762-e3d7-4edd-83cc-6a8eb7bee65b',
+        firstName: 'diego',
+        lastName: 'avellaneda',
+        title: 'Mr',
+        roleId: 1,
+        primaryEmail: 'd@a.com',
+    } as UserProfileModel,
+];
 
 jest.mock('../../../src/db/models/UserProfile');
 
 const mockedUserProfileModel = mocked(UserProfileModel, true);
 
-mockedUserProfileModel.findOne.mockImplementation(
-    async (findOptions) => {
-
-        if ((findOptions?.where as any).uuid === 'b0605d89-6200-4861-a9d5-258ccb33cbe3') {
-            return defaultUserProfile;
-        }
-
-        return null;
+mockedUserProfileModel.findOne.mockImplementation(async (findOptions) => {
+    if (
+        (findOptions?.where as any).uuid ===
+        'b0605d89-6200-4861-a9d5-258ccb33cbe3'
+    ) {
+        return defaultUsersProfile[0];
     }
-);
+
+    return null;
+});
 
 const FUNCTION_NAME = UserProfileRepository.name;
 
 describe(`${FUNCTION_NAME} handler`, () => {
     test(`${FUNCTION_NAME} Valid case `, async () => {
+        const response = await UserProfileRepository.defaultInstance.findOne({
+            uuid: 'b0605d89-6200-4861-a9d5-258ccb33cbe3',
+        });
 
-        const response = await UserProfileRepository.defaultInstance.findOne({ uuid: 'b0605d89-6200-4861-a9d5-258ccb33cbe3' });
-
-        expect(response).toEqual(defaultUserProfile);
+        expect(response).toEqual(defaultUsersProfile[0]);
     });
 
     test(`${FUNCTION_NAME} Invalid case `, async () => {
-
         try {
             await UserProfileRepository.defaultInstance.findOne({ uuid: '' });
             expect(true).toEqual(false);
         } catch (error: any) {
             expect(error.message).toEqual('Error: userProfile not found');
         }
+    });
+});
+
+describe(`${FUNCTION_NAME} handler`, () => {
+    test(`${FUNCTION_NAME} Valid case `, async () => {
+        const primaryEmail = 'd@a.com';
+
+        mockedUserProfileModel.findAll.mockImplementation(async () => {
+            return [];
+        });
+
+        const response =
+            await UserProfileRepository.defaultInstance.checkUserEmailAvailability(
+                primaryEmail
+            );
+
+        expect(response).toEqual(true);
+    });
+
+    test(`${FUNCTION_NAME} Invalid case `, async () => {
+        const primaryEmail = '';
+
+        mockedUserProfileModel.findAll.mockImplementation(async () => {
+            return defaultUsersProfile;
+        });
+
+        const response =
+            await UserProfileRepository.defaultInstance.checkUserEmailAvailability(
+                primaryEmail
+            );
+
+        expect(response).toEqual(false);
     });
 });
