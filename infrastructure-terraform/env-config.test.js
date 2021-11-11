2059d32bac2582419a326db4f0f8772908c00040
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-11-11 10:51:55 -0500

    Updated vpc config files for sls

--- infrastructure-terraform/env-config.test.js
+++ infrastructure-terraform/env-config.test.js
@@ -1,4 +1,4 @@
-module.exports = {
+test = {
     securityGroupIds: ["sg-06801f1bde5997f48"],
     subnetIds: ["subnet-0c8a41a795b2505fd","subnet-0865ed90afe48e074"],
     apiGatewayId: "z6t54j6tm2",
