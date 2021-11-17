0e8c797569f26e87551f821e7a589ed86bc9f277
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-11-17 15:22:01 -0500

    Configure services to use new API Gateway

--- infrastructure-terraform/env-config.production.js
+++ infrastructure-terraform/env-config.production.js
@@ -1,4 +1,4 @@
-production = {
+module.exports = {
     securityGroupIds: ["sg-0840f1d7e5a5ce185"],
     subnetIds: ["subnet-0561588b07d77d20f","subnet-010b8f84f8a266728"],
     apiGatewayId: "ly9ghzhc4a",
--- infrastructure-terraform/env-config.qa.js
+++ infrastructure-terraform/env-config.qa.js
@@ -1,4 +1,4 @@
-qa = {
+module.exports = {
     securityGroupIds: ["sg-0cc68dc1fd2120fae"],
     subnetIds: ["subnet-0e58861832a604e9c","subnet-048f9df03f7f435b6"],
     apiGatewayId: "4c6aosh5n1",
