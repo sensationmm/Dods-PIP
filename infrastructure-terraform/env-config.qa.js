36b12f6d0a84c9e3a14f9458ee8508f37c99a567
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-11-19 10:52:34 -0500

    Updated terraform configuration [skip-ci]

--- infrastructure-terraform/env-config.qa.js
+++ infrastructure-terraform/env-config.qa.js
@@ -1,7 +1,7 @@
 module.exports = {
     securityGroupIds: ["sg-0cc68dc1fd2120fae"],
     subnetIds: ["subnet-0e58861832a604e9c","subnet-048f9df03f7f435b6"],
-    apiGatewayId: "4c6aosh5n1",
-    apiRootResourceId: "99p8azoogc",
+    apiGatewayId: "fz02gxbz09",
+    apiRootResourceId: "qrx4xwscza",
     frontendURL: "http://pip-qa-fargate-lb-2136712671.eu-west-1.elb.amazonaws.com"
 }
