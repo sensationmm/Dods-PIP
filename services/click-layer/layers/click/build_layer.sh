48a5930af3ba1fb92a314db7a73dcf1052e4c902
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-11-23 05:56:04 -0500

    Provided lambda layer for click library

---
+++ services/click-layer/layers/click/build_layer.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+export PKG_DIR="python"
+
+rm -rf ${PKG_DIR} && mkdir -p ${PKG_DIR}
+
+docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
+    pip3 install -r requirements.txt --no-deps -t ${PKG_DIR}
