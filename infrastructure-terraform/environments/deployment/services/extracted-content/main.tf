cf1c9db90be01091488839b64e97a662fbbff10c
michael-brown-somo <michael.brown@somoglobal.com>
2021-11-23 12:36:12 +0000

    added HOC debates ingestions transform queue

--- infrastructure-terraform/environments/deployment/services/extracted-content/main.tf
+++ infrastructure-terraform/environments/deployment/services/extracted-content/main.tf
@@ -1,7 +1,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 // - Extracted content HOC debates ingestion content transform queue
 // --------------------------------------------------------------------------------------------------------------------
-module "migration-stage-1-sqs-queue" {
+module "hoc-debates-ingestion-content-transform-queue" {
   source      = "./modules/sqs"
   environment = var.environment
   queue_name  = "hoc-debates-ingestion-content-transform-queue"
