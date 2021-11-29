3e8c62b37a9133b97892999dcffc6daa62b658d4
Menelik Williams <menelik.williams@somoglobal.com>
2021-11-29 16:50:49 +0000

    feat(DOD-1107): bearer auth header to all api requests

--- frontend/dods_pip/src/pages/api/[...slug].page.tsx
+++ frontend/dods_pip/src/pages/api/[...slug].page.tsx
@@ -5,13 +5,17 @@ export default withSession(async (req, res) => {
   const { slug } = req.query;
   try {
     // we check that the user exists on GitHub and store some data in session
-    const result = await fetchJson(`${process.env.APP_API_URL}/${slug}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
+    const result = await fetchJson(
+      `${process.env.APP_API_URL}/${slug}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(req.body),
       },
-      body: JSON.stringify(req.body),
-    });
+      req,
+    );
     res.json(result);
   } catch (error) {
     const { response: fetchResponse } = error;
--- frontend/dods_pip/src/pages/api/clientaccount/[uuid]/[...slug].page.tsx
+++ frontend/dods_pip/src/pages/api/clientaccount/[uuid]/[...slug].page.tsx
@@ -22,6 +22,7 @@ export default withSession(async (req, res) => {
         {
           method,
         },
+        req,
       );
 
       res.json(response);
