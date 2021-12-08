dc1d6e3a9d2cf6aca6031ffff6d564f68bedb1a6
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2021-12-08 12:26:23 -0800

    bug(DOD-1185): fix failing accounts fetch

--- frontend/dods_pip/src/lib/fetchJson.tsx
+++ frontend/dods_pip/src/lib/fetchJson.tsx
@@ -1,10 +1,5 @@
 import { NextIronRequest } from './session';
 
-interface CustomError extends Error {
-  response?: Response;
-  data?: Record<string, unknown>;
-}
-
 export type UserResponse = {
   id?: string;
   userId?: string;
@@ -49,24 +44,13 @@ export default async function fetchJson(
       ...args,
     });
 
-    const data = await response
-      .clone()
-      .json()
-      .catch(() => response.text());
+    const data = await response.json();
 
     if (response.ok) {
       return data;
     }
 
-    const error = new Error(response.statusText) as CustomError;
-    error.response = response;
-    error.data = {
-      name: data?.name || 'UnknownException',
-      code: response.status,
-      message: data?.message || 'An error happened. Please try again.',
-    };
-
-    throw error;
+    return response;
   } catch (error) {
     if (!error.data) {
       error.data = { message: `!!${error.message}` };
