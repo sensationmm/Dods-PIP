e0312c1db28c172dd08c33355377e40287f00c15
kenan hancer <kenanhancer@gmail.com>
2021-12-13 14:38:53 +0000

    dod-1217: isActive field is added in error case

--- services/dods-iam/src/domain/interfaces/UserProfile.ts
+++ services/dods-iam/src/domain/interfaces/UserProfile.ts
@@ -2,13 +2,26 @@ export type GetUserInput = {
     userId: string;
 }
 
+export type ClientAccountObj = {
+    uuid: string | undefined;
+    name: string | undefined;
+};
+
 export type GetUserOutput = {
     firstName: string;
     lastName: string;
-    email: string;
-    role: string;
+    primaryEmail: string;
+    role: Object;
     isDodsUser: boolean;
-}
+    secondaryEmail?: string | null;
+    telephoneNumber1: string | null;
+    telephoneNumber2?: string | null;
+    title: string | null;
+    clientAccount: ClientAccountObj;
+    clientAccountId: string | null;
+    isActive: boolean;
+    memberSince: Date;
+};
 
 export interface UserProfile {
     getUser(parameters: GetUserInput): Promise<GetUserOutput>;
