6181d38fc63ac48ff87168d2d83fc08b8e178068
michael-brown-somo <michael.brown@somoglobal.com>
2021-12-14 11:34:05 +0000

    added taxonomy service module with s3 bucket

---
+++ infrastructure-terraform/environments/deployment/services/taxonomy/modules/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {
+  description = "environment for stack"
+  default     = "development"
+}
+
+variable "project" {
+  description = "project name"
+  default     = "pip"
+}
+
+variable "name" {
+  description = "bucket name"
+  default     = "default"
+}
