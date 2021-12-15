a0218699c5e678005d9a13111be514ccb8ba39b4
Juan Riaño <juan.riano@somoglobal.com>
2021-12-15 12:30:30 -0500

    seats only count for active users

--- services/client-account/src/handlers/createTeamMember/createTeamMember.ts
+++ services/client-account/src/handlers/createTeamMember/createTeamMember.ts
@@ -10,79 +10,76 @@ import {
 } from '../../repositories';
 
 import { NewTeamMemberParameters } from '../../domain/interfaces/ClientAccountTeam';
+import { TeamMemberTypes } from '../../domain';
 
-export const createTeamMember: AsyncLambdaMiddleware<NewTeamMemberParameters> =
-    async ({ teamMemberType, userProfile, clientAccountId }) => {
-        const isPrimaryEmailAvailable =
-            await UserProfileRepository.defaultInstance.checkUserEmailAvailability(
-                userProfile.primary_email_address
-            );
-
-        if (!isPrimaryEmailAvailable) {
-            return new HttpResponse(HttpStatusCode.CONFLICT, {
-                success: false,
-                message: `A User already exists with the email: ${userProfile.primary_email_address}`,
-            });
-        }
-
-        const availableSeats =
-            await ClientAccountRepository.defaultInstance.getClientAccountAvailableSeats(
-                clientAccountId
-            );
-
-        if (availableSeats < 1 && teamMemberType === 3) {
-            throw new HttpError(
-                'Client Account has not enough available seats',
-                HttpStatusCode.FORBIDDEN
-            );
-        }
-
-        const createUserResponse =
-            await UserProfileRepository.defaultInstance.createUser({
-                title: userProfile.title,
-                firstName: userProfile.first_name,
-                lastName: userProfile.last_name,
-                primaryEmail: userProfile.primary_email_address,
-                roleId: userProfile.role_id,
-                secondaryEmail: userProfile.secondary_email_address,
-                telephoneNumber: userProfile.telephone_number_1,
-                clientAccountId,
-            });
-
-        if (!createUserResponse) {
-            return new HttpResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, {
-                success: false,
-                message: 'Could not create User Profile',
-            });
-        }
-
-        const userId = createUserResponse.data.uuid;
-
-        await ClientAccountRepository.defaultInstance.addTeamMember({
-            clientAccountId,
-            userId,
-            teamMemberType,
-        });
-
-        await ClientAccountRepository.defaultInstance.UpdateCompletion(
-            clientAccountId,
-            true,
-            3
+export const createTeamMember: AsyncLambdaMiddleware<NewTeamMemberParameters> = async ({
+    teamMemberType,
+    userProfile,
+    clientAccountId,
+}) => {
+    const isPrimaryEmailAvailable =
+        await UserProfileRepository.defaultInstance.checkUserEmailAvailability(
+            userProfile.primary_email_address
         );
 
-
-        if (createUserResponse.success) {
-            console.log(createUserResponse.data);
-        }
-
-        const teamMembers =
-            await ClientAccountRepository.defaultInstance.getClientAccountTeam(
-                clientAccountId
-            );
-
-        return new HttpResponse(HttpStatusCode.OK, {
-            success: true,
-            message: 'Team member was created and added to the client account.',
-            data: teamMembers,
+    if (!isPrimaryEmailAvailable) {
+        return new HttpResponse(HttpStatusCode.CONFLICT, {
+            success: false,
+            message: `A User already exists with the email: ${userProfile.primary_email_address}`,
         });
-    };
+    }
+
+    const availableSeats =
+        await ClientAccountRepository.defaultInstance.getClientAccountAvailableSeats(
+            clientAccountId
+        );
+
+    if (availableSeats < 1 && teamMemberType === TeamMemberTypes.ClientUser) {
+        throw new HttpError(
+            'Client Account has not enough available seats',
+            HttpStatusCode.FORBIDDEN
+        );
+    }
+
+    const createUserResponse = await UserProfileRepository.defaultInstance.createUser({
+        title: userProfile.title,
+        firstName: userProfile.first_name,
+        lastName: userProfile.last_name,
+        primaryEmail: userProfile.primary_email_address,
+        roleId: userProfile.role_id,
+        secondaryEmail: userProfile.secondary_email_address,
+        telephoneNumber: userProfile.telephone_number_1,
+        clientAccountId,
+    });
+
+    if (!createUserResponse) {
+        return new HttpResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, {
+            success: false,
+            message: 'Could not create User Profile',
+        });
+    }
+
+    const userId = createUserResponse.data.uuid;
+
+    await ClientAccountRepository.defaultInstance.addTeamMember({
+        clientAccountId,
+        userId,
+        teamMemberType,
+    });
+
+    await ClientAccountRepository.defaultInstance.UpdateCompletion(clientAccountId, true, 3);
+
+    if (createUserResponse.success) {
+        console.log(createUserResponse.data);
+    }
+
+    const teamMembers = await ClientAccountRepository.defaultInstance.getClientAccountTeam(
+        clientAccountId
+    );
+
+    return new HttpResponse(HttpStatusCode.OK, {
+        success: true,
+        message: 'Team member was created and added to the client account.',
+        data: teamMembers,
+    });
+};
--- services/user-profile/src/handlers/updateUser/updateUser.ts
+++ services/user-profile/src/handlers/updateUser/updateUser.ts
@@ -1,19 +1,43 @@
 import { AsyncLambdaMiddleware, HttpResponse, HttpStatusCode } from '@dodsgroup/dods-lambda';
-import { UpdateUserInput, UserProfileError } from '../../domain';
+import { ClientAccountServiceRepository, UserProfileRepositoryV2 } from '../../repositories';
+import { TeamMemberTypes, UpdateUserInput, UserProfileError } from '../../domain';
 
 import { IamRepository } from '../../repositories/IamRepository';
-import { UserProfileRepositoryV2 } from '../../repositories';
 
 export const updateUser: AsyncLambdaMiddleware<UpdateUserInput> = async (parameters) => {
     try {
         const { isActive } = parameters;
-        const response = await UserProfileRepositoryV2.defaultInstance.updateUser(parameters);
+        const user = await UserProfileRepositoryV2.defaultInstance.getUser({
+            userId: parameters.userId,
+        });
+
+        if (!user) {
+            return new HttpResponse(HttpStatusCode.BAD_REQUEST, {
+                success: false,
+                message: `Error: Can not find user with uuid: ${parameters.userId}`,
+            });
+        }
 
         if (isActive === true) {
-            await IamRepository.defaultInstance.enableUser(response.primaryEmail);
+            if (user.clientAccount.teamMemberType === TeamMemberTypes.ClientUser) {
+                const availableSeats =
+                    await ClientAccountServiceRepository.defaultInstance.getRemainingSeats(
+                        user.clientAccount.uuid!
+                    );
+                if (availableSeats < 1) {
+                    return new HttpResponse(HttpStatusCode.BAD_REQUEST, {
+                        success: false,
+                        message: `Error: Can not activate user because Client Account: ${user.clientAccount.uuid} does not have enough seats.`,
+                    });
+                }
+            }
+
+            await IamRepository.defaultInstance.enableUser(user.primaryEmail);
         } else if (isActive === false) {
-            await IamRepository.defaultInstance.disableUser(response.primaryEmail);
+            await IamRepository.defaultInstance.disableUser(user.primaryEmail);
         }
+
+        const response = await UserProfileRepositoryV2.defaultInstance.updateUser(parameters);
 
         return new HttpResponse(HttpStatusCode.OK, {
             success: true,
