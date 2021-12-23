09ed1208f546f0bcfcd8132c0bc514c427ab780f
kenan hancer <kenanhancer@gmail.com>
2021-12-23 18:23:25 +0000

    dod-1262: dods-lambda header name validation is refactored.

--- dods-lambda/src/validation/awsOpenApiRequestAdapter.ts
+++ dods-lambda/src/validation/awsOpenApiRequestAdapter.ts
@@ -7,7 +7,11 @@ const pickFieldValue = (path: string) => {
 
   const keys = path.split('.');
 
-  return (nestedObj: any) => keys.reduce((obj, key) => (obj && obj[key] !== 'undefined') ? obj[key] : undefined, nestedObj);
+  return (nestedObj: any) => keys.reduce((obj, key) => {
+    const [, value] = obj && Object.entries(obj).find(([k, _]) => k.toLowerCase() === key.toLowerCase()) || [];
+
+    return value;
+  }, nestedObj);
 }
 
 export const awsOpenApiRequestAdapter = async (inputData: APIGatewayEvent): Promise<HttpRequest> => {
@@ -36,7 +40,7 @@ export const awsOpenApiRequestAdapter = async (inputData: APIGatewayEvent): Prom
       switch (parameterObject.in) {
         case 'header': {
           const fv = pickFieldValue(parameterObject.name)(inputData.headers);
-          adaptedRequest.headers[parameterObject.name.toLowerCase()] = fv;
+          adaptedRequest.headers[parameterObject.name] = fv;
           adaptedRequest.rawHeaders[parameterObject.name] = fv;
           break;
         }
