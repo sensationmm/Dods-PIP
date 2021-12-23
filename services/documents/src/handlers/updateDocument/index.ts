58ee651f9fe139cb0e93d9721b8305208f2135b0
Diego <diego.avellaneda@somoglobal.com>
2021-12-23 12:27:40 -0500

    Update Document in service, receive arn and over write

--- services/documents/src/handlers/updateDocument/index.ts
+++ services/documents/src/handlers/updateDocument/index.ts
@@ -1,5 +1,5 @@
 import { buildLambdaFunction } from "@dodsgroup/dods-lambda";
-import { updateDocument } from "./updateDocument";
 import { config } from '../../domain';
+import { updateDocument } from "./updateDocument";
 
-export const handle = buildLambdaFunction(updateDocument, { openApiDocumentPath: config.openApiPath, validateRequests: true, validateResponses: false });
+export const handle = buildLambdaFunction(updateDocument, { openApiDocumentPath: config.openApiPath, validateRequests: false, validateResponses: false });
