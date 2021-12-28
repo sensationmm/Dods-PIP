ef1bbb881674488dbe9ac19a2114b8b353002b34
Kenan Hancer <kenanhancer@enejdas-air.home>
2021-12-28 00:03:11 +0000

    dod-609: createDocument is updated

--- services/documents/src/domain/interfaces/index.ts
+++ services/documents/src/domain/interfaces/index.ts
@@ -1,37 +1,91 @@
 export interface SayLocalHelloParameters {
-    documentId: string;
+  documentId: string;
 }
 
 export interface SayHelloParameters extends SayLocalHelloParameters {
 }
 
 export interface DownstreamEndpoints {
-    sayTurkishHelloEndpointUrl: string;
-    sayEnglishHelloEndpointUrl: string;
-    getFullNameEndpointUrl: string;
+  apiGatewayBaseURL: string;
+  // sayTurkishHelloEndpointUrl: string;
+  // sayEnglishHelloEndpointUrl: string;
+  // getFullNameEndpointUrl: string;
 }
 
 export interface CreateNewDocument {
-    jurisdiction?: string
-    documentTitle: string
-    organisationName?: string
-    sourceReferenceFormat?: string
-    sourceReferenceUri?: string
-    createdBy?: string
-    internallyCreated?: boolean
-    schemaType?: string
-    contentSource: string
-    informationType: string
-    contentDateTime?: string
-    createdDateTime: string
-    ingestedDateTime?: string
-    version?: string
-    countryOfOrigin?: string
-    feedFormat?: string
-    language?: string
-    taxonomyTerms?: object[]
-    originalContent?: string
-    documentContent?: string
+  jurisdiction?: string
+  documentTitle: string
+  organisationName?: string
+  sourceReferenceFormat?: string
+  sourceReferenceUri?: string
+  createdBy?: string
+  internallyCreated?: boolean
+  schemaType?: string
+  contentSource: string
+  informationType: string
+  contentDateTime?: string
+  createdDateTime: string
+  ingestedDateTime?: string
+  version?: string
+  countryOfOrigin?: string
+  feedFormat?: string
+  language?: string
+  taxonomyTerms?: object[]
+  originalContent?: string
+  documentContent?: string
+}
+
+
+
+
+
+export interface AncestorTerm {
+  tagId: string;
+  termLabel: string;
+  rank: number;
+}
+
+export interface TaxonomyTerm {
+  tagId: string;
+  facetType: string;
+  inScheme: Array<string>;
+  termLabel: string;
+  ancestorTerms: Array<AncestorTerm>;
+  alternative_labels: Array<string>;
+}
+
+export interface AggField {
+  topics: Array<string>;
+  people: Array<string>;
+  organizations: Array<string>;
+  geography: Array<string>;
+}
+
+export interface CreateDocumentParametersV2 {
+  documentId: string;
+  jurisdiction: string;
+  documentTitle: string;
+  organisationName: string;
+  sourceReferenceFormat: string;
+  sourceReferenceUri: string;
+  createdBy: string;
+  internallyCreated: boolean;
+  schemaType: string;
+  contentSource: string;
+  contentLocation: string;
+  originator: string;
+  informationType: string;
+  contentDateTime: Date;
+  createdDateTime: Date;
+  ingestedDateTime: Date;
+  version: string;
+  countryOfOrigin: string;
+  feedFormat: string;
+  language: string;
+  aggs_fields: AggField;
+  taxonomyTerms: Array<TaxonomyTerm>;
+  originalContent: string;
+  documentContent: string;
 }
 
 export interface getDocumentParameters {
@@ -43,6 +97,6 @@ export interface CreateDocumentParameters {
 }
 export interface updateDocumentParameters
   extends getDocumentParameters,
-    CreateDocumentParameters {}
+  CreateDocumentParameters { }
 
 export * from "./DocumentStoragePersister";
