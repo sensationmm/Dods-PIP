fdbc06701793a43d156f8d9f77dca7b1f8e0f021
Diego <diego.avellaneda@somoglobal.com>
2021-12-29 14:56:59 -0500

    Removed extra / calling endpoint

--- services/editorial-workflow/src/repositories/DocumentServiceRepository.ts
+++ services/editorial-workflow/src/repositories/DocumentServiceRepository.ts
@@ -10,7 +10,7 @@ export class DocumentServiceRepository implements DocumentServicePersister {
     constructor(private baseURL: string = userProfile) { }
 
     async updateDocument(parameters: any): Promise<Object> {
-        const response = await axios.put(`${this.baseURL}/documents`, parameters);
+        const response = await axios.put(`${this.baseURL}documents`, parameters);
         const { data: { success } } = response;
         return { success };
     }
