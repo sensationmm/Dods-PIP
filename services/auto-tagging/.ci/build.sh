0c09113c121eb3c7a047afd71015ca860b73ff37
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2021-12-30 17:28:01 -0500

    WIP multi-env build for backend services

---
+++ services/auto-tagging/.ci/build.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+# This should specify commands to run tests defined on the parent folder.
+# This script will be called from the parent folder so make the right
+#  assumptions about location of files
+
+echo "No tests defined for ${PWD}."
+exit 0
