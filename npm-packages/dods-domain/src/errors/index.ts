1f718975d3adfb1be48a6b3c54bf9f76db634ea0
kenanhancer <kenanhancer@gmail.com>
2022-01-03 11:58:50 +0000

    dods-model and dods-lambda are moved in npm-packages folder and dods-domain and dods-repositories are published

--- npm-packages/dods-domain/src/errors/index.ts
+++ npm-packages/dods-domain/src/errors/index.ts
@@ -1,23 +1,3 @@
-import { HttpStatusCode } from '../http';
-import { HttpError } from './HttpError';
-
-export class UserProfileError extends HttpError {
-    constructor(message: string | unknown) {
-        super(message, HttpStatusCode.NOT_FOUND);
-        this.name = UserProfileError.name;
-    }
-}
-
-export class ClientAccountError extends HttpError {
-    constructor(message: string | unknown) {
-        super(message, HttpStatusCode.NOT_FOUND);
-        this.name = ClientAccountError.name;
-    }
-}
-
-export class CollectionError extends HttpError {
-    constructor(message: string | unknown) {
-        super(message, HttpStatusCode.NOT_FOUND);
-        this.name = CollectionError.name;
-    }
-}
+export * from './CustomHttpErrors';
+export * from './HttpError';
+export * from './EventBridgeError';
