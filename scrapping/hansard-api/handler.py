8becb87ce9d9d696cd434bc5055ab7371b3c453a
Jaymz Campbell <jaymz@jaymz.eu>
2022-01-06 11:26:49 +0000

    Few tweaks whilst on call

--- scrapping/hansard-api/handler.py
+++ scrapping/hansard-api/handler.py
@@ -8,15 +8,12 @@ import os
 from datetime import datetime
 from json import loads, dumps
 
-from lib.common import Common
-from lib.data_model import DataModel
 from lib.logger import logger
 from lib.configs import Config
-from lib.validation import Validator
 
 
-BUCKET = os.environ["CONTENT_BUCKET"]
-PREFIX = os.environ["KEY_PREFIX"]
+# BUCKET = os.environ["CONTENT_BUCKET"]
+# PREFIX = os.environ["KEY_PREFIX"]
 
 content_type = "Hansard HoC API Feed"
 
@@ -29,10 +26,6 @@ config = Config().config_read(("config.ini"))
 from hansard import import_content
 
 def run(event, context):
-    logger.debug(
-        'Starting scrapping process with BUCKET: "%s", prefix: "%s" ', BUCKET, PREFIX
-    )
-
     try:
         import_content(context["date"], context["house"])
     except Exception:
