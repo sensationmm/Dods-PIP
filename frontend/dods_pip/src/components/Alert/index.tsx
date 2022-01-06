4ca3f4052c422d324d938dcbef08f5587407f51d
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2022-01-06 14:07:25 -0800

    feat(DOD-1020): alert component

---
+++ frontend/dods_pip/src/components/Alert/index.tsx
@@ -0,0 +1,149 @@
+import color from '@dods-ui/globals/color';
+import React from 'react';
+
+import Spacer from '../_layout/Spacer';
+import Button from '../Button';
+import DayPicker, { DayType } from '../DayPicker';
+import { Icons } from '../Icon/assets';
+import Text from '../Text';
+import * as Styled from './Alert.styles';
+
+export interface AlertProps {
+  title: string;
+  searchQueries: number;
+  recipients: number;
+  immediateDelivery: boolean;
+  deliveryDay?: DayType;
+  deliveryTimes?: Array<string>;
+  isConsultant: boolean;
+  onDelete: () => void;
+  onCopy: () => void;
+  onViewSettings: () => void;
+  onViewResults: () => void;
+}
+
+const Alert: React.FC<AlertProps> = ({
+  title,
+  searchQueries,
+  recipients,
+  immediateDelivery = false,
+  deliveryDay,
+  deliveryTimes,
+  isConsultant,
+  onDelete,
+  onCopy,
+  onViewSettings,
+  onViewResults,
+}) => {
+  const [showAllTimes, setShowAllTimes] = React.useState<boolean>(false);
+  return (
+    <Styled.wrapper data-test="component-alert">
+      <Styled.content>
+        <Styled.details>
+          <Text type="h3" headingStyle="title">
+            {title}
+          </Text>
+          <Spacer size={2} />
+          <ul>
+            <Text type="liSmall">
+              Search queries: <strong>{searchQueries}</strong>
+            </Text>
+            <Text type="liSmall">
+              Recipients: <strong>{recipients}</strong>
+            </Text>
+          </ul>
+        </Styled.details>
+        <div>
+          <Styled.deliveryTitle>
+            <Text type="label" bold>
+              Delivery schedule
+              {immediateDelivery && ': '}
+            </Text>
+            {immediateDelivery && <Text color={color.base.greyDark}>&nbsp;Immediate</Text>}
+          </Styled.deliveryTitle>
+          <Spacer size={3} />
+          <DayPicker
+            selected={!immediateDelivery ? deliveryDay : undefined}
+            disabled={immediateDelivery}
+          />
+          {!immediateDelivery && deliveryTimes && (
+            <>
+              <Spacer size={3} />
+              <Styled.times>
+                {deliveryTimes.slice(0, 4).map((time, count) => (
+                  <Styled.time key={`time-${count}`}>
+                    <Text bold>{time}</Text>
+                  </Styled.time>
+                ))}
+                {deliveryTimes.length > 4 && (
+                  <Styled.timeAdded
+                    onMouseEnter={() => setShowAllTimes(true)}
+                    onMouseLeave={() => setShowAllTimes(false)}
+                  >
+                    <Text color={!showAllTimes ? color.base.greyDark : color.base.white}>
+                      +{deliveryTimes.slice(4).length}
+                    </Text>
+                  </Styled.timeAdded>
+                )}
+              </Styled.times>
+              {showAllTimes && [
+                <Spacer size={2} key="extra-spacer" />,
+                <Styled.times key="extra-times">
+                  {deliveryTimes.slice(4).map((time, count) => (
+                    <Styled.time key={`time-${count}`}>
+                      <Text bold>{time}</Text>
+                    </Styled.time>
+                  ))}
+                </Styled.times>,
+              ]}
+            </>
+          )}
+        </div>
+      </Styled.content>
+      <Styled.actions>
+        <div>
+          {isConsultant && (
+            <>
+              <Button
+                type="text"
+                isSmall
+                inline
+                label="Delete"
+                icon={Icons.Bin}
+                onClick={onDelete}
+              />
+              <Button
+                type="text"
+                isSmall
+                inline
+                label="Copy to"
+                icon={Icons.Copy}
+                onClick={onCopy}
+              />
+            </>
+          )}
+        </div>
+        <Styled.actionsButtons>
+          <Button
+            type="secondary"
+            isSmall
+            inline
+            label="View settings"
+            icon={Icons.Pencil}
+            onClick={onViewSettings}
+          />
+          <Button
+            isSmall
+            inline
+            label="View Results"
+            icon={Icons.ChevronRightBold}
+            iconAlignment="right"
+            onClick={onViewResults}
+          />
+        </Styled.actionsButtons>
+      </Styled.actions>
+    </Styled.wrapper>
+  );
+};
+
+export default Alert;
--- frontend/dods_pip/src/components/DayPicker/index.tsx
+++ frontend/dods_pip/src/components/DayPicker/index.tsx
@@ -1,5 +1,7 @@
+import color from '@dods-ui/globals/color';
 import React from 'react';
 
+import Text from '../Text';
 import * as Styled from './DayPicker.styles';
 
 export type DayType = 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun';
@@ -30,19 +32,22 @@ const DayPicker: React.FC<DayPickerProps> = ({
     { label: 'Sun', value: 'sun' },
   ] as DayConfig[];
 
-  const isDisabled = onClick === undefined || disabled;
-
   return (
-    <Styled.wrapper data-test="component-day-picker" disabled={isDisabled}>
+    <Styled.wrapper data-test="component-day-picker" disabled={disabled}>
       {days.map((day) => (
         <Styled.day
           data-test={`day-${day.value}`}
           key={`day-${day.value}`}
           selected={selected === day.value}
-          disabled={isDisabled}
+          disabled={disabled}
+          clickable={onClick !== undefined}
           onClick={() => onClick && onClick(day.value)}
         >
-          {day.label}
+          <Text
+            color={selected === day.value && !disabled ? color.base.white : color.base.greyDark}
+          >
+            {day.label}
+          </Text>
         </Styled.day>
       ))}
     </Styled.wrapper>
