8b44db2e60cdb3744a35e27aa56aca7b7d091bb5
Breyner Rojas <breyner.rojas@somoglobal.com>
2022-01-12 11:34:15 -0500

    Refactor: rename AlertAttributesStrechted to AlertOutput and move it to dods-repositories

--- npm-packages/dods-repositories/src/CollectionAlertsRepository/domain.ts
+++ npm-packages/dods-repositories/src/CollectionAlertsRepository/domain.ts
@@ -1,4 +1,23 @@
-import { AlertAttributesStrecthed } from "@dodsgroup/dods-model";
+export interface AlertOutput {
+    id: number;
+    uuid: string;
+    collection: { uuid?: string, name?: string } | {}
+    template: { id?: string, name?: string } | {}
+    isSchedule: boolean;
+    title: string;
+    description?: string | null;
+    schedule?: string | null;
+    timezone?: string | null;
+    isActive?: boolean;
+    isPublished?: boolean;
+    lastStepCompleted?: number;
+    isScheduled?: boolean;
+    hasKeywordsHighlight?: boolean;
+    createdBy: { uuid?: string, name?: string, emailAddress?: string } | {}
+    createdAt?: Date;
+    updatedBy: { uuid?: string, name?: string, emailAddress?: string } | {}
+    updatedAt?: Date | null;
+}
 
 export interface SearchCollectionAlertsParameters {
     collectionId: string;
@@ -74,7 +93,7 @@ export interface SearchAlertParameters {
 }
 
 export interface getAlertById {
-    alert: AlertAttributesStrecthed,
+    alert: AlertOutput,
     searchQueriesCount: number,
     recipientsCount: number
 }
