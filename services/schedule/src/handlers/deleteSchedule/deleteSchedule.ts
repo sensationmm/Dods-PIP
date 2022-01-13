2fde41753cf9e8757aed685844c2db96a30b115b
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2022-01-13 16:22:06 -0500

    Kenan's refactor

--- services/schedule/src/handlers/deleteSchedule/deleteSchedule.ts
+++ services/schedule/src/handlers/deleteSchedule/deleteSchedule.ts
@@ -1,6 +1,6 @@
 import { AsyncLambdaHandler, HttpResponse, HttpStatusCode } from '@dodsgroup/dods-lambda';
 import { deleteScheduleParameters } from '../../domain';
-import { ScheduleRepository } from '../../repositories/ScheduleRpository';
+import { ScheduleRepository } from '../../repositories';
 
 export const deleteSchedule: AsyncLambdaHandler<deleteScheduleParameters> = async (data) => {
     await ScheduleRepository.defaultInstance.deleteSchedule(data);
