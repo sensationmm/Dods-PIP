2173b1a6909ef47269ea90aea897da791c619412
michael-brown-somo <michael.brown@somoglobal.com>
2022-01-17 12:45:40 +0000

    added npm ci command

--- services/auto-tagging/.ci/build.sh
+++ services/auto-tagging/.ci/build.sh
@@ -4,6 +4,7 @@
 # This script will be called from the parent folder so make the right
 #  assumptions about location of files
 
+npm ci
 echo "No tests defined for ${PWD}."
 echo "The developer is embarrased and promised to fix this soon."
 exit 0
