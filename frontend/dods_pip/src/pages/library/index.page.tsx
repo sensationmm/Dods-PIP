45950b9bfaddbcea54877ea59901f2560f450054
Dan <dan@voyced.com>
2022-01-27 11:45:52 +0000

    moved getPayload function to separate file

--- frontend/dods_pip/src/pages/library/index.page.tsx
+++ frontend/dods_pip/src/pages/library/index.page.tsx
@@ -5,7 +5,6 @@ import Chips from '@dods-ui/components/Chips';
 import DateFacet, { IDateRange } from '@dods-ui/components/DateFacet';
 import FacetContainer from '@dods-ui/components/FacetContainer';
 import { format } from 'date-fns';
-import esb, { TermQuery } from 'elastic-builder';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
@@ -22,17 +21,9 @@ import Text from '../../components/Text';
 import color from '../../globals/color';
 import fetchJson from '../../lib/fetchJson';
 import { Api } from '../../utils/api';
-import {
-  AggregationsType,
-  BucketType,
-  ExtendedRequestBodySearch,
-  IAggregations,
-  IResponse,
-  ISourceData,
-  QueryObject,
-  QueryString,
-} from './index';
+import { BucketType, IAggregations, IResponse, ISourceData, QueryObject } from './index';
 import * as Styled from './library.styles';
+import getPayload from './utils/getSearchPayload';
 
 enum AggTypes {
   contentSource = 'contentSource',
@@ -46,166 +37,6 @@ enum AggTypes {
   organisationName = 'organisationName',
   group = 'group',
 }
-
-const aggregations: AggregationsType = {
-  topics: {
-    terms: {
-      field: 'aggs_fields.topics',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  people: {
-    terms: {
-      field: 'aggs_fields.people',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  organizations: {
-    terms: {
-      field: 'aggs_fields.organizations.keyword',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  geography: {
-    terms: {
-      field: 'aggs_fields.geography',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  group: {
-    terms: {
-      field: 'organisationName.keyword',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  originator: {
-    terms: {
-      field: 'originator.keyword',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  jurisdiction: {
-    terms: {
-      field: 'jurisdiction',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  informationType: {
-    terms: {
-      field: 'informationType.keyword',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-  contentSource: {
-    terms: {
-      field: 'contentSource.keyword',
-      min_doc_count: 0,
-      size: 500,
-    },
-  },
-};
-
-const getPayload = ({
-  search = '{}',
-  isEU,
-  isUK,
-}: {
-  search: QueryString;
-  isEU?: boolean | unknown;
-  isUK?: boolean | unknown;
-}): { payload?: unknown; parsedQuery?: QueryObject } => {
-  let esbQuery = esb.boolQuery();
-  let parsedQuery;
-
-  try {
-    parsedQuery = typeof search === 'string' ? JSON.parse(search) : {};
-
-    const {
-      searchTerm = '',
-      basicFilters = [],
-      nestedFilters = [],
-      dateRange = {},
-      resultSize = 20,
-      currentPage = 0,
-    }: QueryObject = parsedQuery;
-
-    const phrases = searchTerm.match(/"(.*?)"/g) || []; // Anything in double quotes, e.g "I am a phrase"
-    const regex = new RegExp(`${phrases.join('|')}`, 'g');
-    const words = searchTerm.replace(regex, ''); // remove all phrases and we're left with the words
-
-    if (searchTerm) {
-      esbQuery = esbQuery.must(
-        esb
-          .boolQuery()
-          .should(phrases.map((phrase) => esb.matchPhraseQuery('documentContent', phrase)))
-          .should(phrases.map((phrase) => esb.matchPhraseQuery('documentTitle', phrase)))
-          .should(words ? esb.matchQuery('documentContent', words) : [])
-          .should(words ? esb.matchQuery('documentTitle', words) : []),
-      );
-    }
-
-    let jurisdictionFilter: TermQuery[] = []; // if both isEU and isUK then no need to filter
-
-    if (isEU && !isUK) {
-      jurisdictionFilter = [esb.termQuery('jurisdiction', 'EU')];
-    } else if (!isEU && isUK) {
-      jurisdictionFilter = [esb.termQuery('jurisdiction', 'UK')];
-    }
-
-    // Facets
-    esbQuery = esbQuery
-      .must(
-        basicFilters
-          .map(({ key, value }) => esb.termQuery(`${key}.keyword`, value))
-          .concat(jurisdictionFilter),
-      )
-      .must(
-        nestedFilters.map(({ path, key, value }) =>
-          esb
-            .nestedQuery()
-            .path(path)
-            .query(esb.termQuery(key.includes('termLabel') ? `${key}.keyword` : key, value)),
-        ),
-      );
-
-    return {
-      payload: {
-        ...(
-          esb
-            .requestBodySearch()
-            .query(
-              esbQuery.filter(
-                esb
-                  .rangeQuery('contentDateTime')
-                  // Fallback to beginning of JS time to now
-                  .gte(dateRange.min || format(new Date(0), 'yyyy-MM-dd'))
-                  .lte(dateRange.max || format(new Date(), 'yyyy-MM-dd')),
-              ),
-            )
-            .size(resultSize)
-            .from(currentPage * resultSize) as ExtendedRequestBodySearch
-        )._body,
-        aggregations,
-      },
-      parsedQuery,
-    };
-  } catch (error) {
-    console.error(error);
-  }
-
-  return {
-    payload: {},
-    parsedQuery,
-  };
-};
 
 interface ILibraryProps {
   parsedQuery: QueryObject;
@@ -272,7 +103,7 @@ export const Library: React.FC<ILibraryProps> = ({
     let newQuery: QueryObject = rest;
 
     if (min && max) {
-      newQuery = { ...rest, dateRange: { min, max }, currentPage: 0 };
+      newQuery = { ...rest, dateRange: { min, max }, currentPage: 1 };
     }
 
     router.push(
@@ -464,13 +295,15 @@ export const Library: React.FC<ILibraryProps> = ({
     }
   };
 
-  const recordsPerPage = [
-    { label: '10', value: '10' },
-    { label: '20', value: '20' },
-    { label: '30', value: '30' },
-    { label: '40', value: '40' },
-    { label: '50', value: '50' },
-  ];
+  const recordsPerPage = useMemo(() => {
+    return [
+      { label: '10', value: '10' },
+      { label: '20', value: '20' },
+      { label: '30', value: '30' },
+      { label: '40', value: '40' },
+      { label: '50', value: '50' },
+    ];
+  }, []);
 
   const onPerPageChange = (value: string) => {
     const newResultSize = parseInt(value);
