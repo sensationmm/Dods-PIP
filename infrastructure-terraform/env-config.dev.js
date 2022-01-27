bf62203394315af0bcc0db8c7a6492aa83d0c45c
bitbucket-pipelines <commits-noreply@bitbucket.org>
2022-01-27 08:10:40 +0000

    Updated terraform configuration [skip ci]

--- infrastructure-terraform/env-config.dev.js
+++ infrastructure-terraform/env-config.dev.js
@@ -3,6 +3,5 @@ module.exports = {
     subnetIds: ["subnet-06a9032f3c233a368","subnet-0680c4b06fa0b55ce"],
     apiGatewayId: "hxyqgu6qy7",
     apiRootResourceId: "g6zz3540a6",
-    frontendURL: "http://pip-dev-fargate-lb-1826645209.eu-west-1.elb.amazonaws.com",
-    apiKeys: "BFCC5E2D497644A0F69D3085F316B0BC"
+    frontendURL: "http://pip-dev-fargate-lb-1826645209.eu-west-1.elb.amazonaws.com"
 }
--- infrastructure-terraform/env-config.qa.js
+++ infrastructure-terraform/env-config.qa.js
@@ -3,6 +3,5 @@ module.exports = {
     subnetIds: ["subnet-0e58861832a604e9c","subnet-048f9df03f7f435b6"],
     apiGatewayId: "fz02gxbz09",
     apiRootResourceId: "qrx4xwscza",
-    frontendURL: "http://pip-qa-fargate-lb-2136712671.eu-west-1.elb.amazonaws.com",
-    apiKeys: "BFCC5E2D497644A0F69D3085F316B0BC"
+    frontendURL: "http://pip-qa-fargate-lb-2136712671.eu-west-1.elb.amazonaws.com"
 }
