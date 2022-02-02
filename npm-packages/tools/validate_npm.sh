636f1fe3439f3a97e155d9c9c3d33da86aac785d
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2022-02-02 11:03:16 -0500

    Address Diego's comments

--- npm-packages/tools/validate_npm.sh
+++ npm-packages/tools/validate_npm.sh
@@ -3,9 +3,12 @@
 NPM_PACKAGE=$(cat package.json| jq -r '.name')
 PKG_VERSION=$(cat package.json| jq -r '.version')
 
-FOUND=$(npm view "$NPM_PACKAGE" versions --json | grep -c "$PKG_VERSION")
+VERSIONS=$(npm view "$NPM_PACKAGE" versions --json)
+FOUND=$(echo $VERSIONS | grep -c "$PKG_VERSION")
 
 if [[ $FOUND -gt 0 ]]; then
    echo "ERROR: Version: $PKG_VERSION for $NPM_PACKAGE has been used before."
+   echo "Previous versions for $NPM_PACKAGE are:"
+   echo $VERSIONS | jq -c '.'
    exit 1
 fi
