f43def83c8cd4bba7da18f08c6587e6d8ee768de
Jose Guerra <jose.guerra@dodsgroup.com>
2022-02-04 11:38:09 +0000

    added modal confirmation

--- frontend/dods_pip/src/pages/editorial/article/[[...articleId]]/index.page.tsx
+++ frontend/dods_pip/src/pages/editorial/article/[[...articleId]]/index.page.tsx
@@ -27,6 +27,7 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 
+import Modal from '../../../../components/Modal';
 import EditorialForm, { EditorialFormFields } from './editorial-form';
 import ScheduleModal from './schedule-modal';
 
@@ -45,6 +46,7 @@ export const EditorialCreate: React.FC<EditorialProps> = ({ setLoading, addNotif
   });
   const [tags, setTags] = React.useState<TagsData[]>([]);
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [isAskPublish, setIsAskPublish] = useState<boolean>(false);
   const [fieldData, setFieldData] = useState<EditorialFormFields>({
     content: '',
     title: '',
@@ -340,6 +342,26 @@ export const EditorialCreate: React.FC<EditorialProps> = ({ setLoading, addNotif
       <Head>
         <title>Dods PIP | Editorial Repository | {isEditMode ? 'Edit' : 'Create'}</title>
       </Head>
+
+      {isAskPublish && (
+        <Modal
+          title="Do you want to publish this article?"
+          size="large"
+          buttons={[
+            { label: 'Cancel', type: 'secondary', onClick: () => setIsAskPublish(false) },
+            {
+              label: 'Publish now',
+              type: 'primary',
+              onClick: isEditMode ? onUpdateAndPublish : onSaveAndPublish,
+            },
+          ]}
+          buttonAlignment="right"
+          onClose={() => setIsAskPublish(false)}
+          isDismissible={true}
+        >
+          <Text type="bodyLarge">The article will be pusblished and visible to all clients.</Text>
+        </Modal>
+      )}
 
       <Panel bgColor={color.base.white}>
         <Breadcrumbs
