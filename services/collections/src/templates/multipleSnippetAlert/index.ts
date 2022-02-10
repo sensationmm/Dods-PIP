e7f89a3e1c602dd77c0b3c84731124638e1c63c6
Diego <diego.avellaneda@somoglobal.com>
2022-02-10 16:09:18 -0500

    unit test related process Alert endpoint

--- services/collections/src/templates/multipleSnippetAlert/index.ts
+++ services/collections/src/templates/multipleSnippetAlert/index.ts
@@ -1,84 +1,85 @@
-import { Article, MultipleArticleSection, MultipleEmailTemplateInput } from '../../domain';
-import { readFile, writeFile } from 'fs/promises';
+import { Article, MultipleEmailTemplateInput } from '../../domain';
 
 import HandleBars from 'handlebars';
 import { config } from '../../domain';
+import { readFile } from 'fs/promises';
 
-const date = new Date().toLocaleDateString('en-GB', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-});
+// const date = new Date().toLocaleDateString('en-GB', {
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric',
+//     hour: '2-digit',
+//     minute: '2-digit',
+// });
 
-const articleGenerator = (): Article => {
-    const id = (Math.random() * 10000).toString(10);
-    return {
-        id,
-        date,
-        title: `Test Article #${id}`,
-        contentSource: {
-            icon: './images/image-placeholder-2.png',
-            name: 'HM Revenue & Custom',
-        },
-        informationType: {
-            icon: './images/icon-sm-document.png',
-            name: 'Press Release',
-        },
-        content:
-            'Mary Kelly Foy (City of Durham): To ask the Secretary of State for Education, whether he plans to provide schools in (a) City of Durham and (b) England with extra funding to meet increased heating costs during the 2021-22 academic year. [59057]',
-        url: 'http://www.google.com/',
-    };
-};
+// const articleGenerator = (): Article => {
+//     const id = (Math.random() * 10000).toString(10);
+//     return {
+//         id,
+//         date,
+//         title: `Test Article #${id}`,
+//         contentSource: {
+//             icon: './images/image-placeholder-2.png',
+//             name: 'HM Revenue & Custom',
+//         },
+//         informationType: {
+//             icon: './images/icon-sm-document.png',
+//             name: 'Press Release',
+//         },
+//         content:
+//             'Mary Kelly Foy (City of Durham): To ask the Secretary of State for Education, whether he plans to provide schools in (a) City of Durham and (b) England with extra funding to meet increased heating costs during the 2021-22 academic year. [59057]',
+//         url: 'http://www.google.com/',
+//     };
+// };
 
-const section1: MultipleArticleSection = {
-    title: 'Section #1',
-    icon: './images/image-placeholder.png',
-    articles: [
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-    ],
-};
+// const section1: MultipleArticleSection = {
+//     title: 'Section #1',
+//     icon: './images/image-placeholder.png',
+//     articles: [
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//     ],
+// };
 
-const section2: MultipleArticleSection = {
-    title: 'Section #2',
-    icon: './images/image-placeholder.png',
-    articles: [
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-        articleGenerator(),
-    ],
-};
+// const section2: MultipleArticleSection = {
+//     title: 'Section #2',
+//     icon: './images/image-placeholder.png',
+//     articles: [
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//         articleGenerator(),
+//     ],
+// };
 
-const alertInput: MultipleEmailTemplateInput = {
-    date,
-    headline: 'The Alert',
-    sections: [section1, section2],
-    url: 'testURL'
-};
+// const alertInput: MultipleEmailTemplateInput = {
+//     date,
+//     headline: 'The Alert',
+//     sections: [section1, section2],
+//     url: 'testURL'
+// };
 
 //* Function to test template
-const generateHTMLFile = async (filePath: string) => {
-    try {
-        const body = await getMultipleSnippetEmailBody(alertInput);
-        await writeFile(filePath, body);
-    } catch (error: any) {
-        console.error(error);
-    }
-};
+// const generateHTMLFile = async (filePath: string) => {
+//     try {
+//         const body = await getMultipleSnippetEmailBody(alertInput);
+//         console.log(body, filePath);
+//         //await writeFile(filePath, body);
+//     } catch (error: any) {
+//         console.error(error);
+//     }
+// };
 
 //* Reduce array into mx2 size matrix to fill double column template
 const reduceArticleMatrix = (rows: Article[][], article: Article, index: number) => {
@@ -101,9 +102,9 @@ export const getMultipleSnippetEmailBody = async (
     });
 };
 
-generateHTMLFile(
-    '/Users/Juan.Riano/dev-dods/dodsmlp/services/email-templates/generated-multipleSnippet.html'
-);
+// generateHTMLFile(
+//     '/Users/Juan.Riano/dev-dods/dodsmlp/services/email-templates/generated-multipleSnippet.html'
+// );
 
 export function groupByFunction(xs: any, key: any) {
     return xs.reduce(function (rv: any, x: any) {
