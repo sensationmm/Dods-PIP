d9083a983941bc7fe9ee61951454827a131c3199
Kevin Reynolds <kevin.reynolds@somoglobal.com>
2022-02-10 10:45:19 -1000

    bug(DOD-592): hide clear icon when disabled

--- frontend/dods_pip/src/components/_form/SearchDropdown/index.tsx
+++ frontend/dods_pip/src/components/_form/SearchDropdown/index.tsx
@@ -1,3 +1,5 @@
+import Icon, { IconSize } from '@dods-ui/components/Icon';
+import { Icons } from '@dods-ui/components/Icon/assets';
 import React from 'react';
 import OutsideClickHandler from 'react-outside-click-handler';
 
@@ -90,6 +92,17 @@ const SearchDropdown: React.FC<SearchDropdownProps> = ({
                 {values.find((val) => val.value === value)?.label}
               </Text>
             </Styled.searchValue>
+          )}
+
+          {!isDisabled && isFilter && value && (
+            <Styled.clear
+              onClick={(e) => {
+                e.stopPropagation();
+                onChange('');
+              }}
+            >
+              <Icon src={Icons.Bin} size={IconSize.large} />
+            </Styled.clear>
           )}
         </InputSearch>
 
