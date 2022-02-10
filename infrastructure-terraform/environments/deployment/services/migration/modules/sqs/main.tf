a5ba28b04550fc2f402e6a7f8218a48673c7460e
Chris Parr <chris.parr@somo-m-ldn-020.home>
2022-02-10 14:21:01 +0000

    changed migration SQS queues from FIFO to standard

--- infrastructure-terraform/environments/deployment/services/migration/modules/sqs/main.tf
+++ infrastructure-terraform/environments/deployment/services/migration/modules/sqs/main.tf
@@ -13,9 +13,7 @@ locals {
 }
 
 resource "aws_sqs_queue" "queue" {
-  name                        = "${local.prefix}.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
+  name                        = "${local.prefix}.standard"
 
   tags = local.common_tags
 }
