ec16cfd34226751c7d658e7c952a0f438fa2c79c
Jay Whittaker <jacob.whittaker@somoglobal.com>
2022-02-16 14:13:26 +0100

    Creates ES query from UI query

---
+++ services/search-service/src/handlers/createESQuery/createESQuery.ts
@@ -0,0 +1,14 @@
+import { HttpResponse, HttpStatusCode } from '@dodsgroup/dods-lambda';
+import { CollectionAlertsRepository } from "@dodsgroup/dods-repositories";
+import { APIGatewayProxyResultV2 } from 'aws-lambda';
+import { CreateESQueryParameters } from '../../domain';
+
+export const createESQuery = async (requestPayload: CreateESQueryParameters): Promise<APIGatewayProxyResultV2> => {
+
+    // @ts-ignore
+    const esQuery = await new CollectionAlertsRepository().createElasticQuery({query: requestPayload.queryString})
+
+    return new HttpResponse(HttpStatusCode.OK, {
+        query: esQuery
+    });
+};
