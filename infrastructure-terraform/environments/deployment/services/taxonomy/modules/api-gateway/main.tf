3c92e37c2a125b2f673bf597b47ae5d88b2c1cbd
Giovanni Espinosa <giovanni.espinosa@somoglobal.com>
2022-02-17 10:59:18 -0500

    more terraform

--- infrastructure-terraform/environments/deployment/services/taxonomy/modules/api-gateway/main.tf
+++ infrastructure-terraform/environments/deployment/services/taxonomy/modules/api-gateway/main.tf
@@ -1,22 +1,8 @@
 // --------------------------------------------------------------------------------------------------------------------
-// - API Gateway
+// - API Gateway for taxonomy trees out of S3 bucket
 // --------------------------------------------------------------------------------------------------------------------
 locals {
   main_resource_name = "taxonomy"
-}
-
-resource "aws_api_gateway_rest_api" "taxonomy" {
-  name           = "${var.project}-${var.environment}-${local.main_resource_name}-api"
-  description = "API Gateway to serve taxonomy data from an S3 bucket"
-  api_key_source = "HEADER"
-  binary_media_types = ["*/*"]
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-resource "aws_api_gateway_api_key" "taxonomy-client" {
-  name        = "frontend"
-  description = "API key meant to be used from the frontend"
 }
 
 # Read-only access policy for buckets
@@ -63,29 +49,25 @@ resource "aws_iam_role" "s3_api_gateway_role" {
   EOF
 }
 
-# S3 Access Policy to the API Gateway Role
+# S3 Access Policy for the API Gateway Role
 resource "aws_iam_role_policy_attachment" "s3_policy_attach" {
   role       = aws_iam_role.s3_api_gateway_role.name
   policy_arn = aws_iam_policy.s3_policy.arn
 }
 
-resource "aws_api_gateway_rest_api_policy" "s3-read-only" {
-  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:Get*",
-                "s3:List*"
-            ],
-            "Resource": "*"
-        }
-    ]
+resource "aws_api_gateway_rest_api" "taxonomy" {
+  name               = "${var.project}-${var.environment}-${local.main_resource_name}-api"
+  description        = "API Gateway to serve taxonomy data from an S3 bucket"
+  api_key_source     = "HEADER"
+  binary_media_types = ["*/*"]
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
-EOF
+
+resource "aws_api_gateway_api_key" "taxonomy-client" {
+  name        = "frontend"
+  description = "API key meant to be used from the frontend"
 }
 
 resource "aws_api_gateway_resource" "taxonomy" {
@@ -107,16 +89,107 @@ resource "aws_api_gateway_resource" "taxonomy-geography-tree" {
   path_part   = "tree"
 }
 
-resource "aws_api_gateway_method" "geography-get" {
+resource "aws_api_gateway_method" "get-geography" {
   rest_api_id   = aws_api_gateway_rest_api.taxonomy.id
   resource_id   = aws_api_gateway_resource.taxonomy-geography-tree.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "s3-object-read-geography" {
+resource "aws_api_gateway_integration" "get-s3-geography" {
   rest_api_id = aws_api_gateway_rest_api.taxonomy.id
   resource_id = aws_api_gateway_resource.taxonomy-geography-tree.id
-  http_method = aws_api_gateway_method.geography-get.http_method
+  http_method = aws_api_gateway_method.get-geography.http_method
   type        = "AWS"
+
+  # Included because of this issue: https://github.com/hashicorp/terraform/issues/10501
+  integration_http_method = "GET"
+
+  # See uri description: https://docs.aws.amazon.com/apigateway/api-reference/resource/integration/
+  uri         = "arn:aws:apigateway:${var.aws_region}:s3:path/taxonomy-trees-dev/Geography.json"
+  credentials = aws_iam_role.s3_api_gateway_role.arn
+
 }
+
+resource "aws_api_gateway_method_response" "geography-200" {
+  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
+  resource_id = aws_api_gateway_resource.taxonomy-geography-tree.id
+  http_method = aws_api_gateway_method.get-geography.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Content-Type"     = true
+    "method.response.header.Content-Encoding" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_method_response" "geography-400" {
+  depends_on = [aws_api_gateway_integration.get-s3-geography]
+
+  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
+  resource_id = aws_api_gateway_resource.taxonomy-geography-tree.id
+  http_method = aws_api_gateway_method.get-geography.http_method
+  status_code = "400"
+}
+
+resource "aws_api_gateway_method_response" "geography-500" {
+  depends_on = [aws_api_gateway_integration.get-s3-geography]
+
+  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
+  resource_id = aws_api_gateway_resource.taxonomy-geography-tree.id
+  http_method = aws_api_gateway_method.get-geography.http_method
+  status_code = "500"
+}
+
+resource "aws_api_gateway_integration_response" "geography-200-integration-response" {
+  depends_on = [aws_api_gateway_integration.get-s3-geography]
+
+  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
+  resource_id = aws_api_gateway_resource.taxonomy-geography-tree.id
+  http_method = aws_api_gateway_method.get-geography.http_method
+  status_code = aws_api_gateway_method_response.geography-200.status_code
+
+  response_parameters = {
+    "method.response.header.Content-Type"     = "integration.response.header.Content-Type"
+    "method.response.header.Content-Encoding" = "integration.response.header.Content-Encoding"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "geography-400-integration-response" {
+  depends_on = [aws_api_gateway_integration.get-s3-geography]
+
+  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
+  resource_id = aws_api_gateway_resource.taxonomy-geography-tree.id
+  http_method = aws_api_gateway_method.get-geography.http_method
+  status_code = aws_api_gateway_method_response.geography-400.status_code
+
+  selection_pattern = "4\\d{2}"
+}
+
+resource "aws_api_gateway_integration_response" "geography-500-integration-response" {
+  depends_on = [aws_api_gateway_integration.get-s3-geography]
+
+  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
+  resource_id = aws_api_gateway_resource.taxonomy-geography-tree.id
+  http_method = aws_api_gateway_method.get-geography.http_method
+  status_code = aws_api_gateway_method_response.geography-500.status_code
+
+  selection_pattern = "5\\d{2}"
+}
+
+resource "aws_api_gateway_deployment" "taxonomy-deployment" {
+  depends_on = [aws_api_gateway_integration.get-s3-geography]
+  rest_api_id = aws_api_gateway_rest_api.taxonomy.id
+  stage_name  = var.environment
+}
+
+/*
+resource "aws_api_gateway_stage" "environment" {
+  deployment_id = aws_api_gateway_deployment.taxonomy-deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.taxonomy.id
+  stage_name    = var.environment
+}*/
--- infrastructure-terraform/environments/deployment/services/taxonomy/modules/api-gateway/output.tf
+++ infrastructure-terraform/environments/deployment/services/taxonomy/modules/api-gateway/output.tf
@@ -1,19 +1,12 @@
 output "api_gateway_id" {
-  value = aws_api_gateway_rest_api.backend.id
-}
-
-output "api_root_resource_id" {
-  value = aws_api_gateway_rest_api.backend.root_resource_id
+  value = aws_api_gateway_rest_api.taxonomy.id
 }
 
 output "api_key_front" {
-  value = aws_api_gateway_api_key.frontend-client.value
-}
-
-output "api_key_test" {
-  value = aws_api_gateway_api_key.testing.value
+  value     = aws_api_gateway_api_key.taxonomy-client.value
+  sensitive = true
 }
 
 output "api_gateway_url" {
-  value = "https://${aws_api_gateway_rest_api.backend.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment}"
+  value = "https://${aws_api_gateway_rest_api.taxonomy.id}.execute-api.${var.aws_region}.amazonaws.com/${var.environment}"
 }
