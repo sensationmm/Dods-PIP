818779a543af91d0d95e6294f693cef5dda1825f
Diego <diego.avellaneda@somoglobal.com>
2022-03-08 16:56:12 -0500

    Added api key security to document

--- npm-packages/dods-repositories/src/DocumentRepository/domain.ts
+++ npm-packages/dods-repositories/src/DocumentRepository/domain.ts
@@ -44,7 +44,7 @@ export interface updatePercolatorParameters {
 export interface DocumentPersister {
     publishDocument(lambdaName: string, payload: string): Promise<boolean>;
     updateDocument(parameters: any): Promise<Object>;
-    getDocument(documentARN: string): Promise<Object>;
+    getDocument(documentARN: string, key: string): Promise<Object>;
     getDocumentByArn(documentARN: string): Promise<DocumentPayloadResponse>;
     getDocumentByArnV1(documentARN: string): Promise<DocumentPayloadResponseV1>;
     scheduleWebhook(parameters: ScheduleEditorialRecordParamateres): Promise<object>;
