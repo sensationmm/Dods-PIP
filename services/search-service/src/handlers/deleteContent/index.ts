b44d6d2f003821c00a40618095ca3a48032ab414
Diego <diego.avellaneda@somoglobal.com>
2022-03-16 16:11:08 -0500

    not validate request in delete content

--- services/search-service/src/handlers/deleteContent/index.ts
+++ services/search-service/src/handlers/deleteContent/index.ts
@@ -2,4 +2,4 @@ import { buildLambdaFunction } from "@dodsgroup/dods-lambda";
 import { config } from '../../domain';
 import { deleteContent } from "./deleteContent";
 
-export const handle = buildLambdaFunction(deleteContent, { openApiDocumentPath: config.openApiPath, validateRequests: true, validateResponses: false, validateSecurity: true });
+export const handle = buildLambdaFunction(deleteContent, { openApiDocumentPath: config.openApiPath, validateRequests: false, validateResponses: false, validateSecurity: true });
